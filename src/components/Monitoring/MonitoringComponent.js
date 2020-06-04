f9f0774ffd48ead16e3f654e5ca73c5310bde643
Orlando J. Vilau <ojvilau@gmail.com>
2020-06-03 21:31:11 -0400

    Capitalize folder 'monitoring'

---
+++ src/components/Monitoring/MonitoringComponent.js
@@ -0,0 +1,117 @@
+import React, { useEffect } from 'react';
+import uuid from 'uuid4';
+import { useMediaQuery, Typography, makeStyles, colors } from '@material-ui/core';
+import { withMonitoringContext, useMonitoringContext } from './MonitoringContext';
+import FiltersMonitoringComponent from './FiltersMonitoringComponent';
+import TableComponent from '../table/TableComponent';
+import headMonitoringCells from './headMonitoringCells';
+import RowMonitoringComponent from './RowMonitoringComponent';
+import { useAuthContext } from '../../contexts/AuthContext';
+import { getPropValue } from '../../helpers/utils';
+import PaginationComponent from '../pagination/PaginationComponent';
+import { withCustomPaginationContext, useCustomPaginationContext } from '../pagination/PaginationContext';
+
+const useStyles = makeStyles({
+  hospitalText: {
+    textAlign: 'right',
+    textTransform: 'uppercase',
+    textDecoration: 'underline'
+  },
+  divRoot: {
+    width: '100%',
+    display: 'flex',
+    '& > span': {
+      marginRight: 15
+    }
+  },
+  redSpan: {
+    color: colors.red.A700
+  },
+  yellowSpan: {
+    color: colors.yellow.A700
+  },
+  greenSpan: {
+    color: colors.green.A700
+  }
+});
+
+function LegendTableMonitoring({ total, totalRed = 0, totalYellow = 0, totalGreen = 0 }) {
+  const classes = useStyles();
+  return (
+    <div className={classes.divRoot}>
+      <span className={classes.total}>
+        <strong>Pacientes: </strong> ({total})
+      </span>
+      <span className={classes.alertsSpan}>
+        <strong>Alertas: </strong> ({totalRed})
+      </span>
+      <span className={classes.redSpan}>
+        <strong>Rojos: </strong> ({totalRed})
+      </span>
+      <span className={classes.yellowSpan}>
+        <strong>Amarillos: </strong> ({totalYellow})
+      </span>
+      <span className={classes.greenSpan}>
+        <strong>Verdes: </strong> ({totalGreen})
+      </span>
+    </div>
+  );
+}
+
+function MonitoringComponent() {
+  const { pageSize, offset } = useCustomPaginationContext();
+  const { list, total, loadingList, selected, setSelected, legend, setParams } = useMonitoringContext();
+  const { currentUserProfile } = useAuthContext();
+  const classes = useStyles();
+  const matchXS = useMediaQuery(theme => theme.breakpoints.down('xs'));
+  const matchSM = useMediaQuery(theme => theme.breakpoints.down('sm'));
+  const cells = matchXS
+    ? [headMonitoringCells[0], headMonitoringCells[1]]
+    : (matchSM && [headMonitoringCells[0], headMonitoringCells[1], headMonitoringCells[2], headMonitoringCells[3]]) ||
+      headMonitoringCells;
+
+  useEffect(() => {
+    setParams({ limit: pageSize, offset });
+  }, [pageSize, offset, setParams]);
+
+  return (
+    <>
+      <FiltersMonitoringComponent currentUserProfile={currentUserProfile} />
+      <TableComponent
+        extraText={
+          <>
+            <Typography className={classes.hospitalText} variant="h6">
+              {getPropValue(currentUserProfile, 'hospital.name')}
+            </Typography>
+            <LegendTableMonitoring
+              total={total}
+              totalRed={legend.totalRed}
+              totalYellow={legend.totalYellow}
+              totalGreen={legend.totalGreen}
+            />
+          </>
+        }
+        headCells={cells}
+        list={list}
+        loadingList={loadingList}
+        render={(row, index) => (
+          <RowMonitoringComponent
+            key={uuid()}
+            cells={cells}
+            row={row}
+            index={index}
+            selected={selected}
+            selectRow={setSelected}
+          />
+        )}
+      />
+      <PaginationComponent
+        total={total}
+        first={getPropValue(list[0], 'fullname')}
+        last={getPropValue(list[list.length - 1], 'fullname')}
+      />
+    </>
+  );
+}
+
+export default withMonitoringContext(withCustomPaginationContext(MonitoringComponent));
---
+++ src/components/Monitoring/RowMonitoringComponent.js
@@ -0,0 +1,74 @@
+import React, { memo } from 'react';
+import uuid from 'uuid4';
+import moment from 'moment';
+import { TableRow, TableCell, Typography } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
+import { getPropValue } from '../../helpers/utils';
+import healthyStandards from '../../helpers/healthyStandards';
+import MedicalDetailButtonIcon from '../buttons/MedicalDetailButtonIcon';
+
+function RowMonitoringComponent({ cells = [], row, selectRow }) {
+  const handleRowSelect = id => {
+    selectRow(id);
+  };
+
+  return (
+    <TableRow onClick={() => handleRowSelect(row.id)}>
+      {cells.map(item => {
+        const color =
+          (item.id === 'pressure' &&
+            healthyStandards.pressure(
+              getPropValue(row, `${item.id}.sistolica`),
+              getPropValue(row, `${item.id}.diastolica`)
+            )) ||
+          (item.id === 'inr.INR' && healthyStandards.inr(getPropValue(row, item.id))) ||
+          (item.id === 'weight.weight' && healthyStandards.weight(row.height, getPropValue(row, item.id))) ||
+          (item.id === 'oxygen.heartbeat' && healthyStandards.heartbeat(getPropValue(row, item.id))) ||
+          (item.id === 'glucose.sugarConcentration' && healthyStandards.heartbeat(getPropValue(row, item.id)));
+        return (
+          <TableCell
+            key={uuid()}
+            align={item.numeric ? 'center' : 'inherit'}
+            style={{
+              backgroundColor: color[100],
+              color: color.A700
+            }}
+          >
+            {(item.id === 'pressure' && (
+              <div>
+                {getPropValue(row, `${item.id}.sistolica`) || '-'}
+                <span>/</span>
+                {getPropValue(row, `${item.id}.diastolica`) || '-'}
+              </div>
+            )) ||
+              (item.id === 'latestDate' && (
+                <Typography>
+                  {moment.unix(row[item.id]).isValid() ? moment.unix(row[item.id]).format('DD/MM/YYYY') : '-'}
+                </Typography>
+              )) ||
+              (item.id === 'user.fullname' && (
+                <Typography
+                  component={NavLink}
+                  to={{
+                    pathname: '/detallesclinicos',
+                    state: {
+                      profile: row.user
+                    }
+                  }}
+                >
+                  {getPropValue(row, item.id) || '-'}
+                </Typography>
+              )) || <Typography>{getPropValue(row, item.id) || '-'}</Typography>}
+          </TableCell>
+        );
+      })}
+      <TableCell key={uuid()} align="center">
+        <NavLink to={{ pathname: '/detallesclinicos', state: { profile: row.user } }}>
+          <MedicalDetailButtonIcon />
+        </NavLink>
+      </TableCell>
+    </TableRow>
+  );
+}
+
+export default memo(RowMonitoringComponent);
