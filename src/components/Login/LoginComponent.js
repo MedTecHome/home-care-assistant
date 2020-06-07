f43c6617051e27b6f1767641434f53404ba69386
Yunior Chavez <yunior.chavez@gmail.com>
2020-06-07 03:22:26 -0600

    fix/set enviroment var for develop and master

--- src/components/Login/LoginComponent.js
+++ src/components/Login/LoginComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Form } from 'react-final-form';
+import { Redirect, Link } from 'react-router-dom';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
 import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 import Box from '@material-ui/core/Box';
 import { useAuthContext } from '../../contexts/AuthContext';
 import loginValidate from './AuthFormValidation';
@@ -14,22 +14,20 @@ import ErrorMessageComponent from './ErrorMessageComponent';
 import SaveButton from '../buttons/SaveButton';
 import CustomTextFieldComponent from '../inputs/CustomTextFieldComponent';
 import useCustomStyles from '../../jss/globalStyles';
+import { NAME_APP } from '../../commons/globalText';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="http://localhost:3000/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
+      <Link to="/">{NAME_APP}</Link> {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
 }
 
 function LoginComponent() {
-  const { signInUser } = useAuthContext();
+  const { signInUser, currentUser } = useAuthContext();
 
   const classes = useCustomStyles();
 
@@ -37,6 +35,10 @@ function LoginComponent() {
     await signInUser(value);
     setTimeout(form.reset);
   };
+
+  if (currentUser) {
+    return <Redirect to="/inicio" />;
+  }
 
   return (
     <>
