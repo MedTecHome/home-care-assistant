8969341d230d1bcd8d0629d2de2859d7e6772cb5
Yunior Chavez <yunior.chavez@gmail.com>
2020-06-12 10:36:04 -0700

    #4 add cinical info address and logo

--- src/components/ClinicInfoComponent.js
+++ src/components/ClinicInfoComponent.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, List, ListItem, makeStyles, Collapse, Container, IconButton } from '@material-ui/core';
+import {
+  Typography,
+  List,
+  ListItem,
+  makeStyles,
+  Collapse,
+  IconButton,
+  Avatar,
+  CircularProgress
+} from '@material-ui/core';
 import { KeyboardArrowDown, KeyboardArrowUp } from '@material-ui/icons';
 import { useAuthContext } from '../contexts/AuthContext';
-import { getPropValue, isLocal } from '../helpers/utils';
+import { getPropValue, isLocal, isEmpty } from '../helpers/utils';
 import { getProfileById } from '../services/profiles';
+import { storageFirebase } from '../firebaseConfig';
 
 const useStyles = makeStyles({
   root: {
@@ -16,28 +26,70 @@ const useStyles = makeStyles({
     right: 0,
     zIndex: 1
   },
+  itemList: {
+    display: 'flex',
+    margin: 'auto'
+  },
+  logoImg: {
+    width: props => (props.open ? 64 : 34),
+    height: props => (props.open ? 64 : 34),
+    objectFit: 'cover',
+    margin: 'auto'
+  },
+  itemContent: {
+    padding: '0 10px'
+  },
   itemTitle: {
     width: '100%',
-    textAlign: 'center'
+    marginBottom: 5
   },
   collapseItem: {
     width: '100%'
   },
   collapseContent: {
     width: '100%',
-    display: 'flex',
-    justifyContent: 'space-around',
-    '& > *': {
-      fontSize: '0.839rem'
-    }
+    display: 'grid',
+    gridTemplateColumns: 'repeat(2, 1fr)',
+    gridGap: 5
+  },
+  phonesText: {
+    textAlign: 'end'
+  },
+  addressText: {
+    gridColumn: 'span 2'
   }
 });
+
+function AsyncImageComponent({ id, className }) {
+  const [url, setUrl] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!isEmpty(id)) {
+      setLoading(true);
+      storageFirebase
+        .ref(id)
+        .getDownloadURL()
+        .then(urlLink => {
+          setUrl(urlLink);
+        })
+        .finally(() => setLoading(false));
+    }
+  }, [id]);
+
+  return (
+    <Avatar variant="square" alt="" src={url} className={className}>
+      {loading ? <CircularProgress size={20} /> : null}
+    </Avatar>
+  );
+}
 
 function ClinicInfoComponent() {
   const { currentUser, currentUserProfile, isClinic, isDoctor, isPatient } = useAuthContext();
   const [clinicInfo, setClinicInfo] = useState(null);
   const [open, setOpen] = useState(false);
-  const classes = useStyles();
+
+  const classes = useStyles({ open });
 
   const isLogin = isLocal ? true : !!currentUser;
 
@@ -80,23 +132,29 @@ function ClinicInfoComponent() {
             {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
           </IconButton>
           <ListItem>
-            <Typography className={classes.itemTitle} component="div">
-              {getPropValue(clinicInfo, 'fullname')}
-            </Typography>
-          </ListItem>
-          <ListItem>
-            <Collapse className={classes.collapseItem} in={open} component={Container} maxWidth="sm">
-              <div className={classes.collapseContent}>
-                <Typography>Correo: {getPropValue(clinicInfo, 'email')}</Typography>
-                <Typography>
-                  Teléfonos:{' '}
-                  {[getPropValue(clinicInfo, 'primaryPhone'), getPropValue(clinicInfo, 'primaryPhone')]
-                    .filter(tel => !!tel)
-                    .map()
-                    .join(', ')}
+            <div className={classes.itemList}>
+              <AsyncImageComponent id={getPropValue(clinicInfo, 'logoUrl')} className={classes.logoImg} />
+              <div className={classes.itemContent}>
+                <Typography className={classes.itemTitle} component="div">
+                  {getPropValue(clinicInfo, 'fullname')}
                 </Typography>
+                <Collapse className={classes.collapseItem} in={open}>
+                  <div className={classes.collapseContent}>
+                    <Typography>Correo: {getPropValue(clinicInfo, 'email')}</Typography>
+                    <Typography className={classes.phonesText}>
+                      Teléfonos:
+                      {[getPropValue(clinicInfo, 'primaryPhone'), getPropValue(clinicInfo, 'secondaryPhone')]
+                        .filter(tel => !!tel)
+                        .map(tel => tel)
+                        .join(', ')}
+                    </Typography>
+                    <Typography className={classes.addressText}>
+                      Dirección:{` ${getPropValue(clinicInfo, 'address')}`}
+                    </Typography>
+                  </div>
+                </Collapse>
               </div>
-            </Collapse>
+            </div>
           </ListItem>
         </List>
       ) : null}
